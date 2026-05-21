8046c82d16a101ed0eafbaac926cfd2077b71523
Egor Kozelskij <ekozelskiy@integrotechnologies.ru>
2026-05-21 21:58:37 +0300

    fix(datatable): generate public type declarations

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,45 @@ import vue from '@vitejs/plugin-vue'
 import dts from 'vite-plugin-dts'
 import { novaVitePlugin } from '@endge/nova-compiler'
 
+const publicTypesEntry = `import 'reflect-metadata'
+import NovaDataTableComponent from './vue/NovaDataTable.vue'
+import { NovaDataTableSchema } from './model/types/datatable.types'
+
+export declare const NovaDataTable: typeof NovaDataTableComponent & {
+  Root: typeof NovaDataTableSchema.Root
+}
+
+export {
+  DataTableColumn,
+  DataTableGrouping,
+  DataTablePinnedRows,
+  DataTableScrollbarLayer,
+  Rect,
+  Surface,
+  Text,
+  TextBlock,
+} from './vue/data-table-dsl'
+export { createDataTableStore, DataTableStore } from './model/module/DataTableStore'
+export { registerNovaDataTable } from './ui/root/datatable-root.registry'
+export * from './model/types/datatable.types'
+export * from './model/runtime/datatable-columns'
+export * from './model/runtime/datatable-layout'
+export * from './model/runtime/DataTableServerRowModel'
+export * from './model/runtime/DataTableSummaryEngine'
+export * from './model/runtime/DataTableClipboardFeedback'
+export * from './model/runtime/DataTableTransactionHistory'
+export * from './model/runtime/DataTableCommitController'
+export * from './model/runtime/DataTableFillHandle'
+export * from './model/runtime/DataTableFillMatrix'
+export * from './model/runtime/DataTableAccessibility'
+export * from './model/runtime/DataTableFilterMenu'
+export * from './model/runtime/DataTableColumnSystem'
+export * from './model/runtime/DataTableTreeRows'
+export * from './model/runtime/DataTableDetailRows'
+export * from './model/runtime/DataTableWorkerIndexPipeline'
+export * from './model/runtime/DataTableServerHardening'
+`
+
 export default defineConfig({
   build: {
     lib: {
@@ -39,6 +78,16 @@ export default defineConfig({
     }),
     dts({
       tsconfigPath: './tsconfig.app.json',
+      insertTypesEntry: true,
+      compilerOptions: {
+        noCheck: true,
+        noEmit: false,
+      },
+      beforeWriteFile(filePath, content) {
+        if (filePath.endsWith(`${path.sep}main.d.ts`)) {
+          return { content: publicTypesEntry }
+        }
+      },
       logLevel: 'silent',
     }),
   ],
